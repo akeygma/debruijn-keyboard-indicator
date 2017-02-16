19ea2a1feb48900ef14baea749eec095db7f91fd
gma <akey@ABCB.localdomain>
2017-02-17 00:05:07 +0800

    [red] upload local files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# debruijn-keyboard-indicator
-a hardware that connects to your PC and show your keyboard indicator light
+# De Bruijn keyboard indicator light
+
+Note: this is an old local project share to public, so files are not so tidy.  I'm planning to take a look later, but now these files are as is.
+
+If you are using a wireless keyboard, then it is likely there is no keyboard status light on it. This project help you build a keyboard status light of your own, it is attached to the USB port of your computer and sync with the status (caps lock, num lock, scroll lock) of your keyboard, so you will never mess with these keys.   This keyboard light would change color and looks great in the night.
+
+This status light also use a little bit math called ['De Bruijn sequence'](https://en.wikipedia.org/wiki/De_Bruijn_sequence).   It is awesome to see a piece of math working in this little thing.
+
+HW build guide
+
+To build this thing you need:
+* A 3D printer.
+* A [Teensy2.0 USB development board](https://item.taobao.com/item.htm?spm=a1z09.2.0.0.O0TGUH&id=521711123240&_u=gbdbs20b2e).
+* An [RGB LED module from DFRobot, model name 5050](https://item.taobao.com/item.htm?spm=a1z09.2.0.0.O0TGUH&id=21133243824&_u=gbdbs26352).
+* A [Stepper motor, model name 28YBJ-48](https://item.taobao.com/item.htm?spm=a1z09.2.0.0.O0TGUH&id=15811333148&_u=gbdbs20c58)
+* Some connect cables because you will need to connect the Teensy board with RGB LED module.
+* A usb cable connect the Teensy2.0 board with your PC, choose the length fits you.
+
+Build steps:
+* print each parts under [directory](3D_print_parts), you need to print the following pieces:
+    * a base
+    * a lid
+    * a mask
+    * a rotary
---
+++ keyboard_led/example.c
@@ -0,0 +1,83 @@
+/* Keyboard example for Teensy USB Development Board
+ * http://www.pjrc.com/teensy/usb_keyboard.html
+ * Copyright (c) 2008 PJRC.COM, LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "usb_keyboard.h"
+
+#define LED_CONFIG	(DDRD |= (1<<6))
+#define LED_OFF		(PORTD &= ~(1<<6))
+#define LED_ON		(PORTD |= (1<<6))
+#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
+
+uint8_t number_keys[10]=
+	{KEY_0,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9};
+
+uint16_t idle_count=0;
+
+int main(void)
+{
+	//uint8_t b, d, mask, i, reset_idle;
+	//uint8_t b_prev=0xFF, d_prev=0xFF;
+
+	// set for 16 MHz clock
+	CPU_PRESCALE(0);
+
+        LED_CONFIG;
+        LED_OFF;
+
+	// Initialize the USB, and then wait for the host to set configuration.
+	// If the Teensy is powered without a PC connected to the USB port,
+	// this will wait forever.
+	usb_init();
+	while (!usb_configured()) /* wait */ ;
+
+	// Wait an extra second for the PC's operating system to load drivers
+	// and do whatever it does to actually be ready for input
+	_delay_ms(1000);
+
+	// Configure timer 0 to generate a timer overflow interrupt every
+	// 256*1024 clock cycles, or approx 61 Hz when using 16 MHz clock
+	// This demonstrates how to use interrupts to implement a simple
+	// inactivity timeout.
+	TCCR0A = 0x00;
+	TCCR0B = 0x05;
+	TIMSK0 = (1<<TOIE0);
+
+	while (1) {
+                if ((keyboard_leds & 0x4) ==  0) LED_OFF;
+                else LED_ON;
+		_delay_ms(2);
+	}
+}
+
+// This interrupt routine is run approx 61 times per second.
+// A very simple inactivity timeout is implemented, where we
+// will send a space character.
+ISR(TIMER0_OVF_vect)
+{
+}
+
+
